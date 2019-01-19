4eabb17f5aa1e538842a248ff8470d1efd188707
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2019-01-19 17:28:51 -0500

    on a un graphe !

--- assets/scripts/2-line-charts.js
+++ assets/scripts/2-line-charts.js
@@ -17,6 +17,15 @@
  */
 function createLine(x, y) {
   // TODO: Retourner une ligne SVG (voir "d3.line"). Pour l'option curve, utiliser un curveBasisOpen.
+  
+  // SEE THIS : https://bl.ocks.org/gordlea/27370d1eea8464b04538e6d8ced39e89
+  // AND THIS : http://www.d3noob.org/2016/08/create-simple-line-graph-using-d3js-v4.html
+  // IT SHOULD FREAKING WORK --edit : it works now
+  var line = d3.line()
+    .x(function(d) { console.log(d.date); return x(d.date);  })
+    .y(function(d) { console.log(d.count); return y(d.count); })
+	.curve(d3.curveBasisOpen);
+  return line;
 
 }
 
@@ -31,7 +40,17 @@ function createLine(x, y) {
 function createFocusLineChart(g, sources, line, color) {
   // TODO: Dessiner le graphique focus dans le groupe "g".
   // Pour chacun des "path" que vous allez dessiner, spécifier l'attribut suivant: .attr("clip-path", "url(#clip)").
-
+  
+  // YOU GIVING ME A HEARTSTROKE --edit : it works now
+	g.append("path")
+		.data([ sources[0]["values"] ]) // <= don't forget the [] around data
+		.attr("class", "line")
+		.attr("d", line)
+		.attr("stroke", "#ffab00") // color
+		.attr("clip-path", "url(0)"); // "url(#clip)"); idk
+		
+	console.log(sources[0]["values"]);
+	return g;
 }
 
 /**
