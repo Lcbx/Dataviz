b0fbae30ad51e037d0534da5019e83852b784386
Charles Marois <chmaru@gigl.polymtl.ca>
2019-01-31 09:07:22 -0500

    modifs data range

--- TP3/assets/scripts/1-preproc.js
+++ TP3/assets/scripts/1-preproc.js
@@ -69,8 +69,9 @@ function domainColor(color, data) {
 function domainRadius(r, data) {
   // TODO: Préciser le domaine de l'échelle de la variable "r" em spécifiant comme valeurs extrêmes le minimum et le
   //       maximum des populations des pays.
-
-	var minDomainR = d3.min(data.population);	
-	var maxDomainR = d3.max(data.population);
+  	var population = data.map(function(d){return d.population});
+  	console.log(population);
+	var minDomainR = d3.min(population);	
+	var maxDomainR = d3.max(population);
 	r.domain([minDomainR, maxDomainR]);
 }
