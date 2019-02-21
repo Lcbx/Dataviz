49106e9314a7a3dbcd67454815e214dfebe444aa
Charles-Auguste Marois <charles.marois.quebec@gmail.com>
2019-02-21 11:55:19 -0500

    changer select et disparition

--- TP5/assets/scripts/3-search.js
+++ TP5/assets/scripts/3-search.js
@@ -27,6 +27,6 @@ function search(map, g, districtId, bound, showPanel) {
    */
 
   map.fitBounds(bound, {animate: true, pan: {animate: true, duration: 1, easeLinearity: 0.5}, maxZoom : 8});
-  g.selectAll(".circonscription").attr("class", d => (d.properties.NUMCF == districtId) ? "selected" : "");
+  g.selectAll(".circonscription").classed("selected", d => (d.properties.NUMCF == districtId));
   
 }
