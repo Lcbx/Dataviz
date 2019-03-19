2d3c6cdc096220d5703c2ed9ea1279cbd92d93a6
jmplante <jmplante_1@hotmail.com>
2019-03-18 22:32:57 -0400

    created ful dataset csv file

--- Projet/data/Data treatment/data_treatment.R
+++ Projet/data/Data treatment/data_treatment.R
@@ -1,8 +1,15 @@
 setwd("/home/jean-michel/Documents/École/INF8808/TPs/INF8808-TP/Projet/data/Data treatment")
 
-data <- read.csv("/home/jean-michel/Documents/École/INF8808/TPs/data.csv")
+data <- read.csv("/home/jean-michel/Documents/École/INF8808/TPs/data.csv", stringsAsFactors = FALSE)
 
 data <- data[!(data$Position > 10), ]
+
+for (row in 1:nrow(data)) {
+  url = unlist(strsplit(as.character(data[row, "URL"]), "/"))
+  data[row, "URL"] <- url[length(url)]
+}
+
+colnames(data)[which(names(data) == "URL")] <- "Id"
 
 write.csv(data, "dataTop10.csv", row.names = FALSE)
 
@@ -21,3 +28,8 @@ for(d in UniqueURLs){
 
 write.csv(ids, "SongIds.csv", row.names = FALSE)
 
+for(row in 1:nrow(data)){
+  d = unlist(strsplit(as.character(data[row, "URL"]), "/"))
+  data[row, "URL"] = d[length(d)]
+  #print(d[length(d)])
+}
