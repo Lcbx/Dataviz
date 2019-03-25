8ac206d5b1262836459a29c18d2b25f8e3197042
Temp <temp@temp.temp>
2019-03-25 10:52:48 -0400

    Replaced spaces in indentation with tabs

--- Projet/assets/scripts/main.js
+++ Projet/assets/scripts/main.js
@@ -46,42 +46,42 @@ function addSvgToHtml(selectorString, width, height) {
 		drawTicks(radarChartGroup, radarChartAxes, radarChartConfiguration.scaleTicks);
 	});
 
-    d3.csv("./data/bumpChartData_global.csv").then(function(data) {
-        console.log(data)
-    })
+	d3.csv("./data/bumpChartData_global.csv").then(function (data) {
+		console.log(data)
+	})
 
-    var margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = 900 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    pairId = 0;
+	var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+		width = 900 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom,
+		pairId = 0;
 
-    /*** bump chart scales ***/
-    var xScale = d3.scaleTime()
-        .domain([new Date(2017,0,1), new Date(2017,11,1)])
-        .rangeRound([0, width - margin.right - 50]);
+	/*** bump chart scales ***/
+	var xScale = d3.scaleTime()
+		.domain([new Date(2017, 0, 1), new Date(2017, 11, 1)])
+		.rangeRound([0, width - margin.right - 50]);
 
-    var yScale = d3.scaleLinear()
-    .domain([1, 11])
-    .range([0, height - 50]);
+	var yScale = d3.scaleLinear()
+		.domain([1, 11])
+		.range([0, height - 50]);
 
 	/*** bump chart SVG ***/
 	// use addSvgToHtml?
-    var bumpChartGroup = d3.select("#bumpChartDiv").append("svg")
-        .attr("id", "bumpChartSvg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var bumpChartGroup = d3.select("#bumpChartDiv").append("svg")
+		.attr("id", "bumpChartSvg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%B"));
+	var xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%B"));
 
-    var tickLabels = ["1","2","3","4","5","6","7","8","9","10",">10"];
-    var yAxis = d3.axisLeft(yScale).tickFormat(function(d,i){ return tickLabels[i] });
+	var tickLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", ">10"];
+	var yAxis = d3.axisLeft(yScale).tickFormat(function (d, i) { return tickLabels[i] });
 
-    createAxes(bumpChartGroup, xAxis, yAxis, height, width);
+	createAxes(bumpChartGroup, xAxis, yAxis, height, width);
 
-    var xTangent = 40,
-        yPadding = .3;
+	var xTangent = 40,
+		yPadding = .3;
 
 
 })(d3, localization);
