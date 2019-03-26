cf9bd717517aa41c3a0606adff223478f758489e
jmplante <jmplante_1@hotmail.com>
2019-03-25 23:06:51 -0400

    bump chart implementation

--- Projet/assets/scripts/bumpChart.js
+++ Projet/assets/scripts/bumpChart.js
@@ -5,29 +5,30 @@
  */
 
 /**
- * Create chart axis
+ * Add axes to chart's svg group
  *
- * @param g       
- * @param xAxis
- * @param yAxis 
- * @param height
- * @param width
+ * @param g         		Svg group where the bump chart should be created
+ * @param xAxis     		horizontal axis
+ * @param yAxis     		vertical axis
+ * @param height    		chart height
+ * @param bottomMargin 	chart bottom margin
  */
-function createAxes(g, xAxis, yAxis, height, width) {
+function addAxes(g, xAxis, yAxis, height, bottomMargin) {
   // Axe horizontal
 
-	var padding = 50;
+	var padding = 0;
 
 	g.append("g")
 		.attr("class", "axis")
-		.attr("transform", "translate(40," + (height - padding) + ")")
-        .call(xAxis)
+		.attr("transform", "translate(10," + (height - bottomMargin) + ")")
+				.call(xAxis)
         .selectAll("text")
         .attr("y", 0)
         .attr("x", 10)
         .attr("dy", 12)
         .attr("transform", "rotate(45)")
-        .style("text-anchor", "start");
+				.style("text-anchor", "start");
+				
 /*
 	g.append("text")
 		.attr("class", "axis-label")
@@ -38,8 +39,8 @@ function createAxes(g, xAxis, yAxis, height, width) {
 */	
 	g.append("g")
         .attr("class", "axis")
-        .attr("transform", "translate(40,0)")
-		.call(yAxis);
+        .attr("transform", "translate(10,0)")
+					.call(yAxis);
 /*
 	g.append("text")
 		.attr("class", "axis-label")
@@ -50,4 +51,55 @@ function createAxes(g, xAxis, yAxis, height, width) {
 			return "rotate(-90)"})
         .text("put axis name here");
 */
+}
+
+/**
+ * Create bump chart paths
+ *
+ * @param g     	Svg group where the bump chart should be created
+ * @param data  	data used to create chart
+ * @param x     	horizontal axis scale
+ * @param y     	vertical axis scale
+ * @param height	chart height
+ */
+function createBumpChart(g, data, x, y, height) {
+	var bump = g.append("g")
+			.attr("class", "bump")
+			.attr("transform", "translate(10," + (height - 30) + ")")
+			.selectAll("g")
+			.data(data)
+			.enter()
+			.append("g");
+			
+	bump.append("path")
+		.attr("class", "songPath")
+		.attr("d", createPath);
+
+	function createPath(d) {
+		var heightFactor = (y.range()[1] - y.range()[0])/10;
+		var path = [];
+		var initialPos = 0;
+		if (d[x.domain()[0]] == "") {
+			initialPos = 11;
+		} else {
+			initialPos = d[x.domain()[0]];
+		}
+		//console.log(d["Track.Name"]);
+		//console.log((initialPos-1)*heightFactor);
+		
+		x.domain().forEach(function(b, i) {
+		var a = x.domain()[i];
+		//console.log(d[b]);
+		path.push("L", x(a), ",", y(d[a + "-offset"]), "h", x.bandwidth(), curve(a, b, d));
+		});
+		return path.join("");
+	}
+
+	function curve(a, b, d) {
+		var xTangent = 10;
+		return "C" + (x(a) + xTangent + x.bandwidth()) + "," + y(d[a + "-offset"]) + " "
+				+ (x(b) - xTangent) + "," + y(d[b + "-offset"]) + " "
+				+ x(b) + "," + y(d[b + "-offset"]);
+	}
+
 }
--- Projet/assets/scripts/main.js
+++ Projet/assets/scripts/main.js
@@ -51,50 +51,51 @@ function addSvgToHtml(selectorString, width, height) {
 		drawData(radarChartGroup, radarChartData.averages, radarChartScale, radarChartConfiguration.radius);
 	});
 
-	d3.csv("./data/bumpChartData.csv").then(function (data) {
+    /**************************/
+    /*** bump chart creation ***/
+    /**************************/
 
-        var bump = bumpChartGroup.append("g")
-                    .attr("class", "bump")
-                    .selectAll("g")
-                    .data(data)
-                    .enter()
-                    .append("g");
-
-		console.log(data)
-	})
-
-	var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    /*** layout parameters ***/
+	var margin = { top: 20, right: 20, bottom: 30, left: 20 },
 		width = 900 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom,
-		pairId = 0;
+		height = 500 - margin.top - margin.bottom;
 
 	/*** bump chart scales ***/
-	var xScale = d3.scaleTime()
-		.domain([new Date(2017, 0, 1), new Date(2017, 11, 1)])
-		.rangeRound([0, width - margin.right - 50]);
-
-	var yScale = d3.scaleLinear()
+	var xDomain = ["2017-01-01", "2017-02-01", "2017-03-01", "2017-04-01",
+					"2017-05-01", "2017-06-01", "2017-07-01", "2017-08-01",
+					"2017-09-01", "2017-10-01", "2017-11-01", "2017-12-01"];
+	var xScale = d3.scalePoint()
+		.domain(xDomain)
+		.range([0, width - margin.right - margin.left]),
+	    yScale = d3.scaleLinear()
 		.domain([1, 11])
-		.range([0, height - 50]);
+        .range([0, height - margin.bottom]);
+        
+    /*** bump chart axes ***/
+	var xTickLabels = ["January", "February", "March", "April", "May", "June", "July", 
+						"August", "September", "October", "November", "December"];
+	var xAxis = d3.axisBottom(xScale).tickFormat(function (d, i) { return xTickLabels[i] });
+	var yTickLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", ">10"];
+	var yAxis = d3.axisLeft(yScale).tickFormat(function (d, i) { return yTickLabels[i] });
 
 	/*** bump chart SVG ***/
-	// use addSvgToHtml?
-	var bumpChartGroup = d3.select("#bumpChartDiv").append("svg")
+    // use addSvgToHtml? --> will optimize code at the end
+    var bumpChartGroup = d3.select("#bumpChartDiv")
+        .append("svg")
 		.attr("id", "bumpChartSvg")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
+        .append("g")
+		.attr("id", "bumpChartGroup")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%B"));
 
-	var tickLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", ">10"];
-	var yAxis = d3.axisLeft(yScale).tickFormat(function (d, i) { return tickLabels[i] });
+    d3.csv("./data/bumpChartData.csv").then(function (data) {
 
-	createAxes(bumpChartGroup, xAxis, yAxis, height, width);
-
-	var xTangent = 40,
-		yPadding = .3;
-
+		var dataGlobal = data.filter(d => d.Region == "global");
+        addAxes(bumpChartGroup, xAxis, yAxis, height, margin.bottom);
+        createBumpChart(bumpChartGroup, dataGlobal, xScale, yScale, height);
+        
+    })
 
 })(d3, localization);
