8630f3fa8ffb4bbbbb263a08b87ff839cb6b1b4c
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2019-04-07 14:47:40 -0400

    pretreated data for barchart

--- Projet/data/Data treatment/songFeatures.py
+++ Projet/data/Data treatment/songFeatures.py
@@ -2,11 +2,15 @@ from collections import namedtuple
 header = "Position,Track_Name,Artist,Streams,Id,Date,Region,danceability,energy,key,loudness,speechiness,acousticness,instrumentalness,liveness,valence,tempo,duration_ms\n"
 Entry = namedtuple("Entry", header)
 
+new_header = "name,nTracks,danceability,energy,loudness,speechiness,acousticness,instrumentalness,liveness,valence,tempo\n"
+Artist = namedtuple("Artist", new_header)
+
 with open("completeDataset.csv", "r", encoding="cp819", errors ='replace') as file:
 	# get rid of header
 	file.read(len(header))
 	
-	songList = {}
+	songs = {}
+	artists = {}
 	
 	# treatment of the data
 	for line in file:
@@ -15,11 +19,48 @@ with open("completeDataset.csv", "r", encoding="cp819", errors ='replace') as fi
 		
 		try:
 			song = Entry( *content )
-			if song.Id not in songList:
-				songList[song.Id] = content
+			
+			if song not in songs:
+				songs[song.Id] = song
 		except:
-			# chinese songs can be shunned without fear
+			# chinese songs can be shunned without fear :P
 			pass
 			#print(content)
+			
+	songs = songs.values()
 	
-	#print(songList)
+	for song in songs:
+		if song.Artist not in artists:
+			artists[song.Artist] = Artist(	song.Artist,
+														1,
+														float(song.danceability),
+														float(song.energy),
+														-float(song.loudness),
+														float(song.speechiness),
+														float(song.acousticness),
+														float(song.instrumentalness),
+														float(song.liveness),
+														float(song.valence),
+														float(song.tempo))
+		else:
+			artist = artists[song.Artist]
+			artists[song.Artist] = Artist(	artist.name,
+														artist.nTracks+1,
+														artist.danceability+float(song.danceability),
+														artist.energy+float(song.energy),
+														artist.loudness-float(song.loudness),
+														artist.speechiness+float(song.speechiness),
+														artist.acousticness+float(song.acousticness),
+														artist.instrumentalness+float(song.instrumentalness),
+														artist.liveness+float(song.liveness),
+														artist.valence+float(song.valence),
+														artist.tempo+float(song.tempo))
+	
+	artists = artists.values()
+	
+	with open('artists.csv','wb') as out:
+		out.write(new_header.encode("cp819"))
+		for row in artists:
+			for el in row:
+				out.write( (str(el)+", ").encode("cp819") )
+			out.write("\n".encode("cp819"))
